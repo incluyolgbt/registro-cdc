23bbb1d60ef92dbfc2f9b2442d4d8843981c33f6
Emanuel Estrada Larios <emamex98 [at] gmail.com>
2022-12-12 13:36:14 -0600

    Add Waap Confirmation

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import PhoneInput from "react-phone-number-input";
-import Avatar from "@mui/material/Avatar";
-import CheckIcon from "@mui/icons-material/Check";
-import { green } from "@mui/material/colors";
-import "react-phone-number-input/style.css";
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import PhoneInput from 'react-phone-number-input';
+import Avatar from '@mui/material/Avatar';
+import CheckIcon from '@mui/icons-material/Check';
+import { green } from '@mui/material/colors';
+import 'react-phone-number-input/style.css';
 
 const theme = createTheme();
 
 const Copyright = (props) => {
   return (
     <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
+      variant='body2'
+      color='text.secondary'
+      align='center'
       {...props}
     >
-      {"Copyright © "}
-      <Link color="inherit" href="https://incluyo.lgbt/">
+      {'Copyright © '}
+      <Link color='inherit' href='https://incluyo.lgbt/'>
         Incluyo
-      </Link>{" "}
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 };
@@ -44,61 +44,59 @@ const CustomTextField = React.forwardRef((props, ref) => (
   <TextField
     required
     fullWidth
-    id="phone"
-    name="phone"
-    label="¿Cuál es tu número de WhatsApp?"
+    id='phone'
+    name='phone'
+    label='¿Cuál es tu número de WhatsApp?'
     inputRef={ref}
-    type="tel"
+    type='tel'
     error={props.phoneError}
   />
 ));
 
 const AlertDialog = (props) => {
-
   return (
     <div>
       <Dialog
         open={props.open}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
         onClose={() => {}}
       >
         <Box
           sx={{
             marginTop: 3,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: green[500] }}>
             <CheckIcon />
           </Avatar>
-          <DialogTitle id="alert-dialog-title">Registro enviado</DialogTitle>
+          <DialogTitle id='alert-dialog-title'>Registro enviado</DialogTitle>
         </Box>
 
         <DialogContent sx={{ marginBottom: 2 }}>
-          <DialogContentText id="alert-dialog-description">
+          <DialogContentText id='alert-dialog-description'>
             Hemos recibido tu registro. En breve te estaremos contactando por
             WhatsApp para enviarte los datos de acceso al Círculo de Cofianza.
           </DialogContentText>
         </DialogContent>
-
       </Dialog>
     </div>
   );
 };
 
 export default function SignUp() {
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const [nameError, setNameError] = useState(false);
   const [ageError, setAgeError] = useState(false);
   const [phoneError, setPhoneError] = useState(false);
   const [privacyCheck, setPrivacyCheck] = useState(false);
   const [disableSend, setDisableSend] = useState(true);
-  const [countryCode, setCountryCode] = useState("+52");
+  const [countryCode, setCountryCode] = useState('+52');
   const [source, setSource] = useState('');
 
   const [open, setOpen] = useState(false);
@@ -109,10 +107,10 @@ export default function SignUp() {
     let errorFlag = false;
 
     const form = {
-      name: data.get("firstName"),
-      age: data.get("age"),
+      name: data.get('firstName'),
+      age: data.get('age'),
       countryCode: countryCode,
-      phone: data.get("phone"),
+      phone: data.get('phone'),
       source: source,
     };
 
@@ -132,37 +130,165 @@ export default function SignUp() {
     }
 
     if (errorFlag) {
-      console.log("ERROR");
+      console.log('ERROR');
       return;
     }
 
-    if (form.countryCode === "+52") {
-      form.countryCode = "+521";
-    } else if (form.countryCode === "+54") {
-      form.countryCode = "+549";
+    if (form.countryCode === '+54') {
+      form.countryCode = '+549';
     }
 
-    form.phone = form.countryCode.replace("+", "") + form.phone;
+    form.phone = form.countryCode.replace('+', '') + form.phone;
 
-    fetch("https://incluyocdc-default-rtdb.firebaseio.com/registro.json", {
-      method: "POST",
+    fetch('https://incluyocdc-default-rtdb.firebaseio.com/registro.json', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(form),
     })
       .then((response) => response.json())
       .then(() => {
-        setOpen(true)
+        setOpen(true);
+      })
+      .then(() => {
+        const ACCESS_TOKEN =
+          'EAAGkpnC4TqEBABja13lfJaAzwG35wf2FKnZCCgb2ZARRMwpZBS77u31xkCUAdPGXCSVI92io5mZCZByESg35wNV8WtbmdilfnPJ7LEs6buq2zeABEdZCgZAACDF25LrFvvHQbJDOEKFyHxZBmz48T2nVZCTMyVGsZAnfHzr998ydMZA25bquBrmiAP3imgGvVYSsvg2K2jBzHlwyZBfyIXZCiMmde';
+        const templateName = 'incluyo_cdc_confirmation';
+        const args = [
+          {
+            type: 'body',
+            parameters: [
+              {
+                type: 'text',
+                text: 'amigue',
+              },
+              {
+                type: 'text',
+                text: '13 de diciembre',
+              },
+              {
+                type: 'text',
+                text: '19:00',
+              },
+              {
+                type: 'text',
+                text: '815 225 22799 ',
+              },
+              {
+                type: 'text',
+                text: '698644',
+              },
+            ],
+          },
+          {
+            type: 'button',
+            sub_type: 'url',
+            index: 0,
+            parameters: [
+              {
+                type: 'text',
+                text: '81522522799?pwd=a2E3UmQrYitEU0lieHFKTFlWS2J4UT09',
+              },
+            ],
+          },
+        ];
+        sendTemplateRemiders([[form.name, form.phone]], {
+          token: ACCESS_TOKEN,
+          templateName,
+          args,
+          firstArgIsName: true,
+        });
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error('Error:', error);
       });
   };
 
-  useEffect(() => {
+  // ************* WAAP API *************
 
-    fetch("https://incluyocdc-default-rtdb.firebaseio.com/cdcInfo.json")
+  const generatePromise = (number, token, templateName, args) => {
+    const body = {
+      messaging_product: 'whatsapp',
+      to: number,
+      type: 'template',
+      template: {
+        name: templateName,
+        language: {
+          code: 'es',
+        },
+        components: args,
+      },
+    };
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(body),
+    };
+
+    return fetch(
+      'https://graph.facebook.com/v15.0/107979732160205/messages',
+      options
+    );
+  };
+
+  const sendTemplateRemiders = (contacts, callParams) => {
+    let { token, templateName, args, firstArgIsName } = callParams;
+
+    const numbers = contacts.map((row) => row[1]);
+
+    let errorCount = 0;
+
+    const result = numbers.reduce((acc, number, i) => {
+      let statusCode;
+
+      const resp = acc
+        .then(() => {
+          if (firstArgIsName) {
+            args[0].parameters[0].text = contacts[i][0];
+          }
+          return generatePromise(number, token, templateName, args);
+        })
+        .then((response) => {
+          statusCode = response.status;
+          return response.json();
+        })
+        .then((response) => {
+          if (statusCode === 200) {
+            console
+              .log
+              // `✅ Mensaje enviado a ${contacts[i][0]}, ${contacts[i][1]}`
+              ();
+          } else {
+            console
+              .log
+              // `❌ Error al enviar a ${contacts[i][0]}, ${contacts[i][1]}`
+              ();
+            console.log(`   ${response.error.message}`);
+            errorCount += 1;
+          }
+        })
+        .catch((err) => console.log(err));
+      return resp;
+    }, Promise.resolve());
+
+    result.then(() => {
+      console.log(
+        `\n🟪 Mensaje enviado a ${numbers.length - errorCount} de ${
+          numbers.length
+        } contactos.`
+      );
+    });
+  };
+
+  // ************* WAAP API *************
+
+  useEffect(() => {
+    fetch('https://incluyocdc-default-rtdb.firebaseio.com/cdcInfo.json')
       .then((response) => response.json())
       .then((data) => {
         setDate(data.date);
@@ -172,7 +298,6 @@ export default function SignUp() {
     const urlParams = new URLSearchParams(window.location.search);
     const sourceParam = urlParams.get('source');
     setSource(sourceParam);
-
   }, []);
 
   useEffect(() => {
@@ -185,27 +310,27 @@ export default function SignUp() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+      <Container component='main' maxWidth='xs'>
         <CssBaseline />
 
         <Box
           sx={{
             marginTop: 6,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          <img src="./LogoIncluyoWeb.png" alt="Logo" width="50%" />
+          <img src='./LogoIncluyoWeb.png' alt='Logo' width='50%' />
 
-          <span style={{ textAlign: "center", marginTop: "10px" }}>
-            <Typography component="h1" variant="h5">
+          <span style={{ textAlign: 'center', marginTop: '10px' }}>
+            <Typography component='h1' variant='h5'>
               Registro - Círculo de Confianza
             </Typography>
           </span>
 
-          <span style={{ textAlign: "center", marginTop: "10px" }}>
-            <Typography component="h1" variant="subtitle1">
+          <span style={{ textAlign: 'center', marginTop: '10px' }}>
+            <Typography component='h1' variant='subtitle1'>
               <b>
                 Únete a nuestro espacio seguro digital para personas jóvenes
                 LGBTQ+, organizado por Incluyo.
@@ -214,8 +339,8 @@ export default function SignUp() {
             </Typography>
           </span>
 
-          <span style={{ textAlign: "center", margin: "5px" }}>
-            <Typography component="h1" variant="subtitle1">
+          <span style={{ textAlign: 'center', margin: '5px' }}>
+            <Typography component='h1' variant='subtitle1'>
               Fecha: {date}
               <br />
               Hora: {time}
@@ -223,7 +348,7 @@ export default function SignUp() {
           </span>
 
           <Box
-            component="form"
+            component='form'
             noValidate
             onSubmit={handleSubmit}
             sx={{ mt: 3 }}
@@ -233,10 +358,10 @@ export default function SignUp() {
                 <TextField
                   required
                   fullWidth
-                  id="firstName"
-                  name="firstName"
-                  label="¿Cuál es tu nombre?"
-                  autoComplete="given-name"
+                  id='firstName'
+                  name='firstName'
+                  label='¿Cuál es tu nombre?'
+                  autoComplete='given-name'
                   error={nameError}
                 />
               </Grid>
@@ -245,11 +370,11 @@ export default function SignUp() {
                 <TextField
                   required
                   fullWidth
-                  id="age"
-                  label="¿Qué edad tienes?"
-                  name="age"
-                  autoComplete="age"
-                  type="tel"
+                  id='age'
+                  label='¿Qué edad tienes?'
+                  name='age'
+                  autoComplete='age'
+                  type='tel'
                   error={ageError}
                 />
               </Grid>
@@ -258,17 +383,17 @@ export default function SignUp() {
                 <PhoneInput
                   international
                   countryCallingCodeEditable={false}
-                  defaultCountry="MX"
+                  defaultCountry='MX'
                   value={countryCode}
                   onChange={setCountryCode}
-                  style={{ fontSize: "25px" }}
+                  style={{ fontSize: '25px' }}
                   phoneError={phoneError}
                   inputComponent={CustomTextField}
                 />
               </Grid>
 
               <Grid item xs={12}>
-                <span style={{ color: "gray" }}>
+                <span style={{ color: 'gray' }}>
                   Te enviaremos los datos de acceso a la sesión de Zoom a través
                   de WhatsApp. No compartiremos tu número con nadie.
                 </span>
@@ -278,8 +403,8 @@ export default function SignUp() {
                 <FormControlLabel
                   control={
                     <Checkbox
-                      value="acceptPrivacy"
-                      color="primary"
+                      value='acceptPrivacy'
+                      color='primary'
                       onChange={() => {
                         setPrivacyCheck(!privacyCheck);
                       }}
@@ -287,10 +412,14 @@ export default function SignUp() {
                   }
                   label={
                     <span>
-                      He leido y acepto el {" "}
-                      <a href="https://docs.google.com/document/d/1VqW2c53X3JGKDNXPf1Gujp6CL9p6TsB_H03PrZ5Bd54/edit?usp=sharing" target="_blank">
-                         Aviso de Privacidad
-                      </a>.
+                      He leido y acepto el{' '}
+                      <a
+                        href='https://docs.google.com/document/d/1VqW2c53X3JGKDNXPf1Gujp6CL9p6TsB_H03PrZ5Bd54/edit?usp=sharing'
+                        target='_blank'
+                      >
+                        Aviso de Privacidad
+                      </a>
+                      .
                     </span>
                   }
                 />
@@ -298,9 +427,9 @@ export default function SignUp() {
             </Grid>
 
             <Button
-              type="submit"
+              type='submit'
               fullWidth
-              variant="contained"
+              variant='contained'
               sx={{ mt: 3, mb: 2 }}
               disabled={disableSend}
             >
@@ -308,7 +437,6 @@ export default function SignUp() {
             </Button>
 
             <AlertDialog open={open} setOpen={setOpen} />
-
           </Box>
         </Box>
 
